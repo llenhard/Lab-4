2a2e94e11efda1191a2a6541fb47d33b3b67045c
imsxz <lukelenhard@gmail.com>
2019-04-25 15:34:39 -0400

    lab4

---
+++ Lab4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuing = true;
+            double input;
+
+            do
+            {
+                Console.Write("Enter an integer: ");
+                input = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Number   Squared    Cubed");
+
+                while (input >= 1)
+                {
+                    Console.WriteLine("{0}         {1}         {2}", input, Math.Pow(input, 2), Math.Pow(input, 3));
+                    input--;
+                }
+
+                Console.WriteLine("Enter another? (Y/N)");
+
+                if (Console.ReadLine().ToLower() == "n")
+                {
+                    continuing = false;
+                }
+
+            }
+            while (continuing);
+        }
+    }
+}
